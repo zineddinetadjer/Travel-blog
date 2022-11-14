a7532fa77376e1b69e65cdd06516108328df6e7e
zineddinetadjer <zinedinetadjer1@gmail.com>
2022-11-14 15:48:38 +0100

    setting all the data

---
+++ travel-blog/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import LogoBar from "./components/LogoBar/LogoBar";
+
+function App() {
+  return (
+    <div className="App">
+      <LogoBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ travel-blog/src/data/ToVisite.js
@@ -0,0 +1,46 @@
+import cons from "../Images/Algerie/cons.jpg";
+import makam from "../Images/Algerie/makam.jpg";
+import sifar from "../Images/Algerie/sifar.jpg";
+import chelal from "../Images/Canada/chelal.jpg";
+import lac from "../Images/Canada/lac.jpg";
+import pont from "../Images/Canada/pont.jpg";
+import barca from "../Images/Espagne/barca.jpg";
+import chateau from "../Images/Espagne/chateau.jpg";
+import musee from "../Images/Espagne/musee.jpg";
+import eifel from "../Images/France/eifel.jpg";
+import havre from "../Images/France/havre.jpg";
+import elysee from "../Images/France/elysee.jpg";
+import cim from "../Images/russie/cim.jpg";
+import montain from "../Images/russie/montain.jpg";
+import rose from "../Images/russie/rose.jpg";
+import aya from "../Images/Turquie/aya.jpg";
+import capa from "../Images/Turquie/capa.jpg";
+import clor from "../Images/Turquie/clor.jpg";
+import big from "../Images/uk/big.jpg";
+import bucking from "../Images/uk/bucking.jpg";
+import harry from "../Images/uk/harry.jpg";
+import face from "../Images/Usa/face.jpg";
+import statut from "../Images/Usa/statu.jpg";
+import time from "../Images/Usa/time.jpg";
+
+export const ToVisite = [
+  {
+    id: 01,
+
+    name: "Constantine",
+    image: cons,
+    desc: "Constantine, est une commune du Nord-Est de l'Algérie, chef-lieu de la wilaya de Constantine. Comptant plus de 448 000 habitants, cette métropole est la troisième ville la plus peuplée du pays...",
+  },
+  {
+    id: 01,
+    name: "Maqam Echahid",
+    image: makam,
+    desc: "Maqam Echahid est un monument aux morts surplombant la ville d'Alger, érigé en 1982 à l'occasion du 20e anniversaire de l'indépendance de l'Algérie (5 juillet 1962), en mémoire des chahids...",
+  },
+  {
+    id: 01,
+    name: "Sifar",
+    image: sifar,
+    desc: "La cité de Sifar ou Séfar est située au cœur du massif montagneux du Tassili n’Ajjer, à 2400 km au sud d’Alger et tout près de la frontière libyenne. Séfar qui est parfois surnommée « la ville des pierres »...",
+  },
+];
